f8afd66b2a8d39197390acea55b57a0fc7ab3e71
delmas <azertyazerty1ze@gmail.com>
2023-08-01 00:36:33 +0000

    handler

---
+++ src/main/java/com/delmas/gestiondestock/handler/ErrorDto.java
@@ -0,0 +1,21 @@
+package com.delmas.gestiondestock.handler;
+
+import com.delmas.gestiondestock.Exception.ErrorCodes;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private  Integer httpCode;
+    private ErrorCodes code;
+    private String message;
+    private List<String> errors = new ArrayList<>();
+}
---
+++ src/main/java/com/delmas/gestiondestock/handler/RestExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.delmas.gestiondestock.handler;
+
+import com.delmas.gestiondestock.Exception.EntityNotFoundException;
+import com.delmas.gestiondestock.Exception.InvalidEntityException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception, WebRequest webRequest) {
+
+        final HttpStatus notFound = HttpStatus.NOT_FOUND;
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(notFound.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDto, notFound);
+    }
+
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidEntityException exception, WebRequest webRequest) {
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(badRequest.value())
+                .message(exception.getMessage())
+                .errors(exception.getErrors())
+                .build();
+
+        return new ResponseEntity<>(errorDto, badRequest);
+    }
+}
