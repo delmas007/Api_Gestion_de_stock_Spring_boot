b295a4c2d69ec8d3e993899c60184140103e9e56
delmas <azertyazerty1ze@gmail.com>
2023-08-22 23:24:26 +0000

    swagger

--- src/main/java/com/delmas/gestiondestock/Controller/Api/ArticleApi.java
+++ src/main/java/com/delmas/gestiondestock/Controller/Api/ArticleApi.java
@@ -1,6 +1,10 @@
 package com.delmas.gestiondestock.Controller.Api;
 
 import com.delmas.gestiondestock.dto.ArticleDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +12,14 @@ import java.util.List;
 
 import static com.delmas.gestiondestock.Utils.Contants.APP_ROOT;
 
+@Api(APP_ROOT+"/article")
 public interface ArticleApi {
 
     @PostMapping(value = APP_ROOT+"/article/create", consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Enregistrer un article",notes = "Cette methode permet d'enregistrer ou modifier un ",response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "L'objet article cree/modifier")
+    })
     ArticleDto save(@RequestBody ArticleDto dto);
 
     @GetMapping(value = APP_ROOT+"/article/{idArticle}",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,4 +33,5 @@ public interface ArticleApi {
 
     @DeleteMapping(value = APP_ROOT+"/article/delete/{idArticle}")
     Void delete(@PathVariable("idArticle") Integer id);
+
 }
