87e5790598b6b2eb5e19a725bff9f14430298612
delmas <azertyazerty1ze@gmail.com>
2023-08-30 00:58:24 +0000

    swagger

--- src/main/java/com/delmas/gestiondestock/Controller/Api/ArticleApi.java
+++ src/main/java/com/delmas/gestiondestock/Controller/Api/ArticleApi.java
@@ -18,17 +18,32 @@ public interface ArticleApi {
     @PostMapping(value = APP_ROOT+"/article/create", consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Enregistrer un article",notes = "Cette methode permet d'enregistrer ou modifier un ",response = ArticleDto.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200,message = "L'objet article cree/modifier")
+            @ApiResponse(code = 200,message = "L'objet article cree/modifier"),
+            @ApiResponse(code = 400,message = "L'objet article n'est pas valide")
     })
     ArticleDto save(@RequestBody ArticleDto dto);
 
     @GetMapping(value = APP_ROOT+"/article/{idArticle}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Rechercher un article par ID",notes = "Cette methode permet de chercher un article par son ID ",response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "L'article a ete trouve dans la BDD"),
+            @ApiResponse(code = 404,message = "Aucun article n'existe dans la BDD avec l'ID fourni")
+    })
     ArticleDto findById(@PathVariable("idArticle") Integer id);
 
     @GetMapping(value = APP_ROOT+"/article/{codeArticle}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Rechercher un article par CODE",notes = "Cette methode permet de chercher un article par son CODE ",response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "L'article a ete trouve dans la BDD"),
+            @ApiResponse(code = 404,message = "Aucun article n'existe dans la BDD avec le CODE fourni")
+    })
     ArticleDto findByCodeArticle(@PathVariable("codeArticle")String codeArticle);
 
     @GetMapping(value = APP_ROOT+"/article/all",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Renvoie la liste des articles",notes = "Cette methode permet de chercher un article par son CODE ",response = ArticleDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "L'article a ete trouve dans la BDD")
+    })
     List<ArticleDto> findAll();
 
     @DeleteMapping(value = APP_ROOT+"/article/delete/{idArticle}")
