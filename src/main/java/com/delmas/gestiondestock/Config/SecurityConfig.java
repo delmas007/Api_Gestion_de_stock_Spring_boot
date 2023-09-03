8a6dc9642f72cc836330e41d46298ad4cddff85c
delmas <azertyazerty1ze@gmail.com>
2023-09-03 22:57:39 +0000

    swagger

--- src/main/java/com/delmas/gestiondestock/Config/SecurityConfig.java
+++ src/main/java/com/delmas/gestiondestock/Config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
